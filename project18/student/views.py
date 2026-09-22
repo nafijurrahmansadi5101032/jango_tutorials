5d2c5b8a7cf838a9021720200ccb7a331fb59d79
Nafijur Rahman Sadi <233580453+nafijurrahmansadi5101032@users.noreply.github.com>
2026-09-22 22:56:24 +0600

    Add files via upload

---
+++ project18/student/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import StudentForm
+from .models import Student
+
+def student_create(request):
+    form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'student_success.html')
+    
+    return render(request, 'student_form.html', {'form': form})
+
+def students_list(request):
+    students = Student.objects.all()
+    return render(request, 'students_list.html', {'students': students})
+
+def student_detail(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, 'student_detail.html', {'student': student})
+
+def student_edit(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    form = StudentForm(instance=student)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('students_list')
+        else:
+            form = StudentForm(instance=student)
+    
+    return render(request, 'student_form.html', {'form': form})
+
+def student_delete(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('students_list')
+    
+    return render(request, 'student_confirm_delete.html', {'student': student})
